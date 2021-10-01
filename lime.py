28b4348cd947702601804d1d13bdbb2e945460eb
lcrosvila <34547568+lcrosvila@users.noreply.github.com>
2021-10-01 10:47:53 +0200

    Ongoing work...

--- lime.py
+++ lime.py
@@ -18,16 +18,17 @@ C0 = A4 * pow(2, -4.75)
 note_names = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
 note_names_octave = [note+str(i) for i in range(11) for note in note_names]
 
-
 class LIME:
-    def __init__(self, config, spice):
-        self.spice = spice
+    def __init__(self, config, model):
+        self.model_type = config["model_type"]
+        self.model = model
 
         self.num_segments = config["num_segments"]
         self.num_perturb = config["num_perturb"]
         self.kernel_width = config["kernel_width"]
         self.num_top_features = config["num_top_features"]
-
+        
+        self.pred_precision = config["pred_precision"]
 
         self.model_load_path = config["model_load_path"]
         self.original_audio_path = config["audio_path"]
@@ -50,7 +51,7 @@ class LIME:
         return {"img": np.abs(perturbed_image), "audio": perturbed_signal}
 
     def load_model(self):
-        self.model = self.spice.model
+        self.model.model
 
     def get_stft(self, audio_samples):
         return {"img": np.abs(librosa.stft(audio_samples,  n_fft=2048)), "audio": audio_samples}
@@ -63,9 +64,10 @@ class LIME:
 
         for pert in perturbations:
             perturbed_img = self.perturb_signal(Xi["audio"], pert, superpixels)
-            _, _, pred = self.spice.get_predictions(perturbed_img["audio"])
+            _, _, pred = self.model.get_predictions(perturbed_img["audio"])
             predictions.append(pred)
-
+        
+        if 
         predictions = np.array(predictions)[:,np.newaxis,:]
         original_image = np.ones(num_superpixels)[np.newaxis,:] #Perturbation with all superpixels enabled 
         distances = sklearn.metrics.pairwise_distances(perturbations,original_image, metric='cosine').ravel()
@@ -74,12 +76,22 @@ class LIME:
 
         return superpixels, perturbations, predictions, weights
     
+    def find_nearest(array, value):
+        return [[p] for p in array if abs(p-value) < self.pred_precision]
+    
     def get_top_bottom(self, audio_samples, name_class_to_explain):
-        class_to_explain = self.note_names_octave.index(name_class_to_explain)
         Xi = self.get_stft(audio_samples)
         superpixels, perturbations, predictions, weights = self.get_perturbations(Xi)
         simpler_model = LinearRegression()
-        simpler_model.fit(X=perturbations, y=predictions[:,:,class_to_explain], sample_weight=weights)
+        
+        if "spice" in self.model_type:
+            class_to_explain = self.note_names_octave.index(name_class_to_explain)
+            simpler_model.fit(X=perturbations, y=predictions[:,:,class_to_explain], sample_weight=weights)
+        else:
+            class_to_explain = 
+            simpler_model.fit(X=perturbations, y=predictions[:,:,class_to_explain], sample_weight=weights)
+            
+        
         coeff = simpler_model.coef_[0]
         top_features = np.argsort(coeff)[-self.num_top_features:] 
 
